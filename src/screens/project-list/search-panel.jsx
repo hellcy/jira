fe03a53561d226894d8fff56f3d77f2d04448d40
Yuan Cheng <chengyuan82281681@hotmail.com>
2022-03-01 00:13:51 +1100

    feat: added search-panel.jsx

---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+export const SearchPanel = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch("").then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [param]);
+
+  return (
+    <form action="">
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(event) =>
+            setParam({
+              ...param,
+              name: event.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(event) =>
+            setParam({
+              ...param,
+              personId: event.target.value,
+            })
+          }
+        >
+          <option value={""}>负责人</option>
+          {users.map((user) => (
+            <option value={user.id}>{user.name}</option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};
