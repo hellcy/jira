f2ee847987d2bf01203107de4b3a69ae7eeb1450
Yuan Cheng <ycheng@atlassian.com>
2022-03-08 12:44:16 +1100

    feat: 使用useAsync处理me API的异步请求，并处理loading和error

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
 
 // 为了使元素保持垂直居中，我们需要将Row的所有子元素的上下margin去掉
 // styled component可以传入参数
@@ -22,3 +24,23 @@ export const Row = styled.div<{
         : undefined};
   }
 `;
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center; // 水平居中
+  align-items: center; // 垂直居中
+`;
+
+export const FullPageLoading = () => (
+  <FullPage>
+    <Spin size={"large"} />
+  </FullPage>
+);
+
+export const FullPageErrorFallback = ({ error }: { error: Error | null }) => (
+  <FullPage>
+    <DevTools />
+    <Typography.Text type={"danger"}>{error?.message}</Typography.Text>
+  </FullPage>
+);
--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode } from "react";
 import * as auth from "auth-provider";
 import { User } from "../screens/project-list/search-panel";
 import { http } from "../utils/http";
 import { useMount } from "../utils";
+import { useAsync } from "../utils/use-async";
+import { FullPageErrorFallback, FullPageLoading } from "../components/lib";
 
 interface AuthForm {
   username: string;
@@ -38,7 +40,15 @@ AuthContext.displayName = "AuthContext";
  * @constructor
  */
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
 
   const login = (form: AuthForm) =>
     auth.login(form).then((user) => setUser(user));
@@ -47,12 +57,20 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   // 我们可以简写成只保留函数名，取消参数
   // 这在Java语言中也有体现，method reference
   const register = (form: AuthForm) => auth.register(form).then(setUser);
-  const logout = () => auth.logout().then((user) => setUser(null));
+  const logout = () => auth.logout().then(() => setUser(null));
 
   // 初始化user
   useMount(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser());
   });
+
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
 
   return (
     <AuthContext.Provider
