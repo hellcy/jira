dda47eb75a83bdc72c908df3cb914885d7c1785b
Yuan Cheng <ycheng@atlassian.com>
2022-03-11 16:46:09 +1100

    feat: 使用id-select管理Select组件的id类型

---
+++ src/components/id-select.tsx
@@ -0,0 +1,43 @@
+import { Raw } from "../types";
+import { Select } from "antd";
+import React from "react";
+
+type SelectProps = React.ComponentProps<typeof Select>;
+
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "onChange" | "options"> {
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+/**
+ * 这个组件的目的是统一项目中的select
+ * 即不管传入的value是什么类型的值，
+ * 在onChange函数中只接受 number | undefined 类型的值
+ * 当传入的值无法被转换成number时，返回undefined
+ * @param props
+ * @constructor
+ */
+export const IdSelect = (props: IdSelectProps) => {
+  const { value, onChange, defaultOptionName, options, ...restProps } = props;
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option key={option.id} value={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
